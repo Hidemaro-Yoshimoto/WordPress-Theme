85466335b680e106c3a7b094f955cca36460c928
Hidemaro-Yoshimoto <bsnow83ball@gmail.com>
2023-06-03 19:44:55 +0900

    コピペ用のショートコードリファレンスを作成。

---
+++ libs/shortcode_example.php
@@ -0,0 +1,34 @@
+<!-- 吹き出し -->
+[hukidashi]ショートコードのテキストですショートコードのテキストです[/hukidashi]
+
+<!-- Point指定のボックス -->
+[point_commentary]ここにテキストが入ります。[/point_commentary]
+
+<!-- 自由に決められるのボックス -->
+[comment_box ttl=TIPS content=コメントのテキスト]
+
+<!-- 背景の塗りつぶし -->
+[nuritsubushi content=背景色の塗りつぶしショートコード bgc=#efefef color=red]
+
+<!-- 付箋風 -->
+[husen bgc=gray border=#ccc content=付箋風の囲みタグ color=white]
+
+<!-- 会話形式 右から左 -->
+[kaiwa_right_to_left]ここに会話の内容を入力します。[/kaiwa_right_to_left]
+
+<!-- 会話形式 左から右 -->
+[kaiwa_left_to_right]左から右に出る吹き出しです。[/kaiwa_left_to_right]
+
+<!-- リスト ボックスシャドウ -->
+[list_boxshadow list1=リストアイテム1 list2=リストアイテム2]
+
+[list_boxshadow list1=リストアイテム1 list2=リストアイテム2 list3=リストアイテム3]
+
+[list_boxshadow wrap=ol list1=リストアイテム1 list2=リストアイテム2 list3=リストアイテム3 list4=リストアイテム4]
+
+[list_boxshadow wrap=ol list1=リストアイテム1 list2=リストアイテム2 list3=リストアイテム3 list4=リストアイテム4 list5=リストアイテム5]
+
+<!-- リスト 鉛筆アイコン -->
+[list_pencil_icon list1=リストアイテム1 list2=リストアイテム2]
+
+[list_pencil_icon list1=リストアイテム1 list2=リストアイテム2 list3=リストアイテム3  list4=リストアイテム4]
